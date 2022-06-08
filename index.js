afc4835e802b12b260e6d9b7513773bb0ce779d4
Hyrum Sorensen <hyrumgary@gmail.com>
2022-06-08 15:22:31 -0600

    Final Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//console.log('Hello World')
+const message = document.querySelector('#message');
+function addMovie(event) {
+    event.preventDefault();
+    let inputField = document.querySelector('input');
+    const movie = document.createElement('li');
+    const movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie);
+    movie.appendChild(movieTitle);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+    document.querySelector('ul').appendChild(movie);
+    inputField.value = "";
+}
+document.querySelector('form').addEventListener('submit',addMovie);
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
+    message.textContent = event.target.previousSibling.textContent + " was deleted!";
+    revealMessage();
+}
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked');
+    if (event.target.classList.contains('checked')){
+        message.textContent = event.target.textContent + " watched!";
+    }else{
+        message.textContent = event.target.textContent + " added back!";
+    }
+    revealMessage();
+}
+function addHide(){
+    message.classList.add("hide");
+}
+function revealMessage(){
+    message.classList.remove("hide");
+    setTimeout(addHide, 1000);
+}
